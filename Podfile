2178b130cbf06b344cd7492061ec54de0b3b2b3b
Wassim S Seifeddine <wassim_seifeddine95@live.com>
2016-08-11 21:20:48 +0300

    Installed socketIO

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment this line to define a global platform for your project
+ platform :ios, '9.0'
+
+target 'Socket Chat' do
+  # Comment this line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Socket Chat
+   pod 'Socket.IO-Client-Swift'
+  target 'Socket ChatTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Socket ChatUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- Socket Chat/ViewController.swift
+++ Socket Chat/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import SocketIOClientSwift
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let socket = SocketIOClient(socketURL: NSURL(string: "http://localhost:8080")!, options: [.Log(true), .ForcePolling(true)])
+        
+        socket.on("connect") {data, ack in
+            print("socket connected")
+        }
+        
+        socket.on("currentAmount") {data, ack in
+            if let cur = data[0] as? Double {
+                socket.emitWithAck("canUpdate", cur)(timeoutAfter: 0) {data in
+                    socket.emit("update", ["amount": cur + 2.50])
+                }
+                
+                ack.with("Got your currentAmount", "dude")
+            }
+        }
+        
+        socket.connect()
     }
 
     override func didReceiveMemoryWarning() {

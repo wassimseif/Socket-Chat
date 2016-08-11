f74900b039b916b28acb929085ace13ef9f64696
Wassim S Seifeddine <wassim_seifeddine95@live.com>
2016-08-11 21:15:23 +0300

    Initial Commit

---
+++ Socket Chat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Socket Chat
+//
+//  Created by Wassim Seifeddine on 8/11/16.
+//  Copyright © 2016 Wassim Seifeddine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
